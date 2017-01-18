c8ba241994255483fb2421266b98a1d90dc3012c
Ana Luiza Ferrer <analcferrer@gmail.com>
2017-01-18 14:52:31 -0200

    Initial Commit

---
+++ AulaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AulaGit
+//
+//  Created by Ana Luiza Ferrer on 18/01/17.
+//  Copyright © 2017 Ana Luiza Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
